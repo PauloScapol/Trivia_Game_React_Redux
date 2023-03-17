c457f40995a95bda11d0e1acda8f0560fd7f84b5
bruno <bruno.costa1203@yahoo.com>
2023-03-17 16:03:36 -0300

    Requisito 3 completo

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Settings extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1
+          data-testid="settings-title"
+        >
+          Settings
+
+        </h1>
+      </div>
+    );
+  }
+}
