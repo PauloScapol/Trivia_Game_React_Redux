5a185f0d2e46e8276d6d2855ff0c2067298931d8
bruno <bruno.costa1203@yahoo.com>
2023-03-17 16:19:03 -0300

    Requisito 5 completo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import logo from '../trivia.png';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
 class Header extends React.Component {
+  state = {
+    hash: '',
+  };
+
+  componentDidMount() {
+    const { email } = this.props;
+    const emailHash = md5(email).toString();
+    this.setState({ hash: emailHash });
+  }
+
   render() {
+    const { name } = this.props;
+    const { hash } = this.state;
+    console.log(name);
     return (
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
+      <header>
+        <img data-testid="header-profile-picture" src={ `https://www.gravatar.com/avatar/${hash}` } alt={ name } />
+        <h2 data-testid="header-player-name">{name}</h2>
+        <h2 data-testid="header-score">0</h2>
       </header>
     );
   }
 }
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.player.gravatarEmail,
+  name: state.player.name,
+});
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+export default connect(mapStateToProps)(Header);
